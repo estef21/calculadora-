2f8d70992f8ad44813a06efa99af39ef8768955b
estef21 <chama1292.ep@gmail.com>
2019-09-12 19:26:03 -0500

    calculadora

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php 
+
+include "calculadora.php";
+
+calculadora::$numero=4;
+
+/*calculadora::$potencia=3;*/
+
+calculadora::divisor=3;
+
+/*calculadora::_potencia();*/
+
+calculadora::residuo();
+
+ ?>
